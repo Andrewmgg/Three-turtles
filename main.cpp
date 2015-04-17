ba381b4862fc2a8afae168522eb4affb1fce52ee
z4p <z4p@tut.by>
2015-04-18 02:00:42 +0300

    int64 fix ;=)

--- main.cpp
+++ main.cpp
@@ -2,77 +2,73 @@
 #include <cstdlib>
 #include <math.h>
 #define temp 100
+#define int64 unsigned long long
 using namespace std;
 
 class RSA
 {
 private:
-    int p;
-    int q;
+    int64 p;
+    int64 q;
+    int64 N, M;
+    
 public:
 
-    RSA(int p, int q)
+    RSA(int64 p, int64 q)
     {
         this->p = p;
         this->q = q;
+
+        this->N = p * q;
+        this->M = (p - 1) * (q - 1);
     }
 
-    long n()
+    int64 e()
     {
-        return p * q;
-    }
-
-    int m()
-    {
-        return (p - 1) * (q - 1);
-    }
-
-    int e()
-    {
-        int e;
+        int64 e;
         do
         {
-            //int degree = (int)(random() % 5);
+            //int64 degree = (int64)(random() % 5);
             //e = (1 << degree) + 1;
             e = 3;
         }
-        while(nod(e, m()) > 1);
+        while(nod(e, this->M) > 1);
         return e;
     }
 
-    int d()// работает если существует обратный элемент
+    int64 d()// работает если существует обратный элемент
     {
-        int res = 0;
-        int e = this->e();
-        int m = this->m();
-        while((e * res) % m != 1)
+        int64 res = 0;
+        int64 e = this->e();
+        //int m = this->m();
+        while((e * res) % this->M != 1)
             res++;
         return res;
     }
 
-    int nod(int a, int b)
+    int64 nod(int64 a, int64 b)
     {
-        while (a && b)
-
+        while (a && b) {
             if (a >= b)
                 a %= b;
             else
                 b %= a;
+        }
         return a | b;
     }
 
-    long encode(int text)
+    int64 encode(int64 text)
     {
-        long res = 1;
-        for (int n = this->n(), e = this->e(), i = 0; i < e; ++i)
+        int64 res = 1;
+        for (int n = this->N, e = this->e(), i = 0; i < e; ++i)
             res = (res * text) % n;
         return res;
     }
 
-    int decode(long ctext)
+    int64 decode(int64 ctext)
     {
-        int res = 1;
-        for (int n = this->n(), d = this->d(), i = 0; i < d; ++i)
+        int64 res = 1;
+        for (int64 n = this->N, d = this->d(), i = 0; i < d; ++i)
             res = (res * ctext) % n;
         return res;
     }
@@ -81,16 +77,16 @@ public:
 
 int main()
 {
-    int p,q;
+    int64 p,q;
     p = 3557;
     q = 2579;
     //cin>>p>>q;
     RSA rsa(p, q);
-    int text;
+    int64 text;
     cin >> text;
-    long cod = rsa.encode(text);
+    int64 cod = rsa.encode(text);
     cout << cod << endl;
-    int decod = rsa.decode(cod);
+    int64 decod = rsa.decode(cod);
     cout << decod << endl;
 
 
