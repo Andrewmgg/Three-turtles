d08ec51b746d1487a518e0d4c54976ce3011a37c
Andrei Pazniak <krucios@mail.ru>
2015-04-19 17:42:07 +0300

    [~] C-8

--- C-8/main.cpp
+++ C-8/main.cpp
@@ -29,16 +29,16 @@ int nod(int a, int b)
 
 int f_euler_rec(int n)
 {
-    int ans = n;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0) {
-            while (n % i == 0) n /= i;
-            ans -= ans / i;
-        }
-        if (n > 1) ans -= ans / n;
-    }
-    return ans;
+	int result = n;
+	for (int i=2; i*i<=n; ++i)
+		if (n % i == 0) {
+			while (n % i == 0)
+				n /= i;
+			result -= result / i;
+		}
+	if (n > 1)
+		result -= result / n;
+	return result;
 }
 
 int main()
