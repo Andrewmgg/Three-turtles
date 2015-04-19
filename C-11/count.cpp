c002ccd9e9f3735d4b98d15dd30bf7bc28344c56
z4p <z4p@tut.by>
2015-04-19 18:21:12 +0300

    almost all C-11

--- C-11/count.cpp
+++ C-11/count.cpp
@@ -8,8 +8,23 @@ using namespace std;
 double stdC[26] = {8.17, 1.49, 2.78, 4.25, 12.7, 2.23, 2.02, 6.09, 6.97, 0.15, 0.77, 4.03, 2.41, 6.75, 7.51, 1.93, 0.01, 5.99, 6.33, 9.06, 2.76, 0.98, 2.36, 0.15, 1.97, 0.07};
 
 double cnt[26];
-int d;
-string res;
+
+void drawHex(unsigned char *c, int l)
+{
+    string s = "";
+    for (int i = 0; i < l; i++) {
+        char lw = c[i] % 0x10;
+        if (lw < 10) lw += '0';
+        else lw += 'A'-10;
+
+        char hg = c[i] / 0x10;
+        if (hg < 10) hg += '0';
+        else hg += 'A'-10;
+        s += hg;
+        s += lw;
+    }
+    cout << s << endl;
+}
 
 char h2c(char h) {
     if (h >= '0' && h <='9')
@@ -25,13 +40,13 @@ char toupper(char c) {
     return c;
 }
 
-string Vizener(char* s, int sL, char* key, int keyL) {
+string Vizener(char *s, int sL, unsigned char *key, int keyL) {
     string res = "";
     int j = 0;
     for (int i = 0; i < sL; ++i) {
-        char dc = s[i] ^ (char)(key[j]);
+        char dc = s[i] ^ key[j];
         res += dc;
-        j = (j + 1) % keyL;
+        j = (j+1) % keyL;
     }
     
     return res;
@@ -40,16 +55,16 @@ string Vizener(char* s, int sL, char* key, int keyL) {
 double summ() {
     double s = 0;
     for (int i = 0; i < 26; ++i)
-        s += abs(stdC[i] - cnt[i])*cnt[i];
+        s += abs(stdC[i] - cnt[i])*1000;
     return s;
 }
 
 int main() {
     string s;
-    //std::getline(std::cin, s);
+    std::getline(std::cin, s);
     
-    s = "F96DE8C227A259C87EE1DA2AED57C93FE5DA36ED4EC87EF2C63AAE5B9A7EFFD673BE4ACF7BE8923CAB1ECE7AF2DA3DA44FCF7AE29235A24C963FF0DF3CA3599A70E5DA36BF1ECE77F8DC34BE129A6CF4D126BF5B9A7CFEDF3EB850D37CF0C63AA2509A76FF9227A55B9A6FE3D720A850D97AB1DD35ED5FCE6BF0D138A84CC931B1F121B44ECE70F6C032BD56C33FF9D320ED5CDF7AFF9226BE5BDE3FF7DD21ED56CF71F5C036A94D963FF8D473A351CE3FE5DA3CB84DDB71F5C17FED51DC3FE8D732BF4D963FF3C727ED4AC87EF5DB27A451D47EFD9230BF47CA6BFEC12ABE4ADF72E29224A84CDF3FF5D720A459D47AF59232A35A9A7AE7D33FB85FCE7AF5923AA31EDB3FF7D33ABF52C33FF0D673A551D93FFCD33DA35BC831B1F43CBF1EDF67F0DF23A15B963FE5DA36ED68D378F4DC36BF5B9A7AFFD121B44ECE76FEDC73BE5DD27AFCD773BA5FC93FE5DA3CB859D26BB1C63CED5CDF3FE2D730B84CDF3FF7DD21ED5ADF7CF0D636BE1EDB79E5D721ED57CE3FE6D320ED57D469F4DC27A85A963FF3C727ED49DF3FFFDD24ED55D470E69E73AC50DE3FE5DA3ABE1EDF67F4C030A44DDF3FF5D73EA250C96BE3D327A84D963FE5DA32B91ED36BB1D132A31ED87AB1D021A255DF71B1C436BF479A7AF0C13AA14794";
-    //string key = "BA1F91B253CD3E";
+    //s = "F96DE8C227A259C87EE1DA2AED57C93FE5DA36ED4EC87EF2C63AAE5B9A7EFFD673BE4ACF7BE8923CAB1ECE7AF2DA3DA44FCF7AE29235A24C963FF0DF3CA3599A70E5DA36BF1ECE77F8DC34BE129A6CF4D126BF5B9A7CFEDF3EB850D37CF0C63AA2509A76FF9227A55B9A6FE3D720A850D97AB1DD35ED5FCE6BF0D138A84CC931B1F121B44ECE70F6C032BD56C33FF9D320ED5CDF7AFF9226BE5BDE3FF7DD21ED56CF71F5C036A94D963FF8D473A351CE3FE5DA3CB84DDB71F5C17FED51DC3FE8D732BF4D963FF3C727ED4AC87EF5DB27A451D47EFD9230BF47CA6BFEC12ABE4ADF72E29224A84CDF3FF5D720A459D47AF59232A35A9A7AE7D33FB85FCE7AF5923AA31EDB3FF7D33ABF52C33FF0D673A551D93FFCD33DA35BC831B1F43CBF1EDF67F0DF23A15B963FE5DA36ED68D378F4DC36BF5B9A7AFFD121B44ECE76FEDC73BE5DD27AFCD773BA5FC93FE5DA3CB859D26BB1C63CED5CDF3FE2D730B84CDF3FF7DD21ED5ADF7CF0D636BE1EDB79E5D721ED57CE3FE6D320ED57D469F4DC27A85A963FF3C727ED49DF3FFFDD24ED55D470E69E73AC50DE3FE5DA3ABE1EDF67F4C030A44DDF3FF5D73EA250C96BE3D327A84D963FE5DA32B91ED36BB1D132A31ED87AB1D021A255DF71B1C436BF479A7AF0C13AA14794";
+    string key1 = "BA1F91B253CD3E";
 
     // HEX-строку в массив чаров
     int stL = s.size() / 2;
@@ -94,18 +109,19 @@ int main() {
         //cout << m;
     }
     
-    cout << maxL << endl;
+    //cout << maxL << endl;
     
     
     
     
     // подбираем ключ
-    char key[13];
+    unsigned char key[13];
+    
+    char *a = new char[stL];
     
     for (int i = 0; i < keyL; i++) {
-        cout << i << ": ";
+        //cout << i << ": ";
         // составляем массив символов, шифруемых одним символом ключа
-        char *a = new char[maxL];
         int aL = 0;
         for(int j = 0; j < stL; j+=keyL) {
             if (j+i < stL) {
@@ -128,7 +144,7 @@ int main() {
                 if ( !isprint(c) ) goto label; // если хоть одна буква - непечатная, то ключ негодный
                 c = toupper(c);
                 if ( c >= 'A' && c <= 'Z' )
-                    cnt[ c ] += 100.0 / stL;
+                    cnt[ c-'A' ] += 100.0 / (double)stL;
             }
                 
             c = summ();
@@ -139,13 +155,20 @@ int main() {
         label: if (0) {}
         }
         
-        cout << mins << endl;
+        //cout << mK << endl;
         key[i] = mK;
         
-        delete a;
     }
+    delete a;
 
-    cout << key << endl << Vizener(st, stL, key, keyL);
+    unsigned char key2[7] = {0xBA, 0x1F, 0x91, 0xB2, 0x53, 0xCD, 0x3E};
+    for (int i = 0; i < 14; i+=2) {
+        //key2[i/2] = (int)h2c(key1[i])*16 + h2c(key1[i+1]);
+    }
+    
+    drawHex(key, 7);
+    //drawHex(key2, 7);
+    cout << Vizener(st, stL, key, keyL);
     
     delete st;
     return 0;
